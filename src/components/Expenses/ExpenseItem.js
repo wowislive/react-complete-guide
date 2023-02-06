26c2b42bf4038d2af2fbbabc3fe7570d13b8bfe3
Dmitrii Pavlov <wowislive@gmail.com>
2023-02-06 20:51:56 +0200

    new component folders. useState to EI.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenceDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => setTitle("Updated!");
+
   return (
     <Card className="expense-item">
       <ExpenceDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
