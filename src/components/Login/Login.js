2b8567ace4f98b85e815da390fef33cc158ffde5
Dmitrii Pavlov <wowislive@gmail.com>
2023-07-07 21:46:44 +0300

    study useContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -49,28 +49,29 @@ const Login = (props) => {
     };
   }, []);
 
-  /* useEffect(() => {
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
-      console.log("Check Validity");
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
+      console.log("Check form Validity");
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 1000);
 
     return () => {
       console.log("CLEANUP");
       clearTimeout(identifier);
     };
-  }, [enteredEmail, enteredPassword]); */
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
-    setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
+    /* setFormIsValid(event.target.value.includes("@") && passwordState.isValid); */
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "USER_INPUT", val: event.target.value });
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    /* setFormIsValid(emailState.isValid && event.target.value.trim().length > 6); */
   };
 
   const validateEmailHandler = () => {
