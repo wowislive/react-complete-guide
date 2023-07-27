566abf18cc0c43428a6e859fc0f1fbdbac2f1669
Dmitrii Pavlov <wowislive@gmail.com>
2023-07-27 20:50:26 +0300

    chapter 19 complete. chapter 20 start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
-
 function App() {
-  const isVisible = useSelector((state) => state.cart.isVisible);
-  return (
-    <Layout>
-      {isVisible && <Cart />}
-      <Products />
-    </Layout>
-  );
+  return <div></div>;
 }
 
 export default App;
