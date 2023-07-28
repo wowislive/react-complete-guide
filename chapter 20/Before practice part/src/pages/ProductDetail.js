def38d640b00b3a66bd6810209053353e0ed678b
Dmitrii Pavlov <wowislive@gmail.com>
2023-07-28 19:16:29 +0300

    chapter 20 start of practice part

---
+++ chapter 20/Before practice part/src/pages/ProductDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+
+const ProductDetailPage = () => {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Product Detail</h1>
+      <p>{params.productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default ProductDetailPage;
