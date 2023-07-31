1e8e645583162df43056d751da2389b96e6b83d2
Dmitrii Pavlov <wowislive@gmail.com>
2023-07-31 20:30:03 +0300

    chapter 20 end

--- chapter 20/src/components/EventsNavigation.js
+++ chapter 20/src/components/EventsNavigation.js
@@ -1,49 +1,37 @@
 import { NavLink } from 'react-router-dom';
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from './EventsNavigation.module.css';
 
-function MainNavigation() {
+function EventsNavigation() {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
             <NavLink
-              to="/"
+              to="/events"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
               end
             >
-              Home
+              All Events
             </NavLink>
           </li>
           <li>
             <NavLink
-              to="/events"
+              to="/events/new"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
             >
-              Events
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/newsletter"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Newsletter
+              New Event
             </NavLink>
           </li>
         </ul>
       </nav>
-      <NewsletterSignup />
     </header>
   );
 }
 
-export default MainNavigation;
+export default EventsNavigation;
