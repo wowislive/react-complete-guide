3b6bda514e4b2f658525d7ab75fbd5252197311c
Dmitrii Pavlov <wowislive@gmail.com>
2023-08-07 19:42:49 +0300

    Auth chapter continue

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -35,7 +35,10 @@ export async function action({ request }) {
     throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
 
-  //manage token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
 
   return redirect("/");
 }
